4036265a748ce4d77e784a1755211c95479fde06
qfafard <152772443+qfafard@users.noreply.github.com>
2023-12-19 02:10:45 -0600

    Create Selection.cs
    Allows object selection on objects with the tag "Selectable"

---
+++ Selection.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selection : MonoBehaviour
+{
+    public Material highlightMaterial;
+    public Material selectionMaterial;
+
+    private Material originalMaterialHighlight;
+    private Material originalMaterialSelection;
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+    private GameObject gameMenu;
+
+    private void Start()
+    {
+        gameMenu = GameObject.Find("TestMenu");
+        gameMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Highlight
+        if (highlight != null)
+        {
+            highlight.GetComponent<MeshRenderer>().sharedMaterial = originalMaterialHighlight;
+            highlight = null;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+        {
+            highlight = raycastHit.transform;
+            if (highlight.CompareTag("Selectable") && highlight != selection)
+            {
+                if (highlight.GetComponent<MeshRenderer>().material != highlightMaterial)
+                {
+                    originalMaterialHighlight = highlight.GetComponent<MeshRenderer>().material;
+                    highlight.GetComponent<MeshRenderer>().material = highlightMaterial;
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+
+        // Selection
+        if (Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (highlight)
+            {
+                if (selection != null)
+                {
+                    selection.GetComponent<MeshRenderer>().material = originalMaterialSelection;
+                }
+                selection = raycastHit.transform;
+                if (selection.GetComponent<MeshRenderer>().material != selectionMaterial)
+                {
+                    originalMaterialSelection = originalMaterialHighlight;
+                    selection.GetComponent<MeshRenderer>().material = selectionMaterial;
+                }
+                highlight = null;
+
+                gameMenu.SetActive(true);
+            }
+            else
+            {
+                if (selection)
+                {
+                    selection.GetComponent<MeshRenderer>().material = originalMaterialSelection;
+                    selection = null;
+                }
+                gameMenu.SetActive(false);
+            }
+        }
+
+    }
+
+}
